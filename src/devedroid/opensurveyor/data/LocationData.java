01750c2f7ff06d0e0f9375c8c8ab8510d9c190b0
Pavel Melnikov <positron96@gmail.com>
2013-08-23 16:16:06 +0700

    + Added logi for logcat + added altitude support for LocationData

--- src/devedroid/opensurveyor/data/LocationData.java
+++ src/devedroid/opensurveyor/data/LocationData.java
@@ -5,14 +5,17 @@ import java.io.Serializable;
 import java.io.Writer;
 import java.util.Locale;
 
+import org.osmdroid.util.GeoPoint;
+
 public class LocationData implements Serializable{
 	
-	public final double lat, lon, heading;
+	public final double lat, lon, heading, alt;
 	
 	public LocationData(LocationData loc) {
 		lat = loc.lat;
 		lon = loc.lon;
 		heading = loc.heading;
+		alt = loc.alt;
 		
 	}
 	
@@ -23,9 +26,15 @@ public class LocationData implements Serializable{
 			heading = loc.getBearing();
 		else
 			heading = Double.NaN;
+		if(loc.hasAltitude())
+			alt = loc.getAltitude();
+		else
+			alt = Double.NaN;
 	}
 	
 	public boolean hasHeading() { return !Double.isNaN(heading); }
+	
+	public boolean hasAltitude() { return !Double.isNaN(alt); }
 	
 	
 	public void writeLocationTag(Writer w) throws IOException {
@@ -34,7 +43,15 @@ public class LocationData implements Serializable{
 				lat, lon));
 		if(hasHeading()) w.append(String.format(Locale.US, 
 				"heading=\"%.2f\" ",heading));
+		if(hasAltitude()) w.append(String.format(Locale.US, 
+				"alt=\"%.2f\" ",alt));
 		w.append("/>\n");
+	}
+	
+	public GeoPoint getGeoPoint() {
+		GeoPoint res = new GeoPoint(lat,lon);
+		if(hasAltitude()) res.setAltitude((int) alt);
+		return res;
 	}
 
 }
